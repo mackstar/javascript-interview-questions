24467b7ff4002910781c2b73a61f9f23968b1882
Tom Keeber <tkeeber@hotmail.com>
2017-10-05 09:23:08 +0100

    Update 7_bind_call_apply.js

--- src/questions/core-javascript/7_bind_call_apply.js
+++ src/questions/core-javascript/7_bind_call_apply.js
@@ -21,14 +21,13 @@ var a1 = new Animal('Dog')
 var sayName = p1.sayName
 
 // 1) use the sayName function for any of p1. p2 or a1 (printing out a message)
-sayName.call(p1,  'JavaScript Rocks!') // call has comma seperated arguments
 
 
 // 2) Are there any other ways of archieving this? 
-sayName.apply(p1, ['JavaScript Rocks!']) //apply has an array of arguments. 
 
-
+// 3) In this code... 
 var boundSayName = sayName.bind(p1, 'JavaScript Rocks') // create a new function entirely. Bound to the given arguements.
 boundSayName()
 
+ // What will this do?
 boundSayName.apply(p2)
