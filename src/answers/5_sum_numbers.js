671b5994a42e53808f484ce1cc029956ea2e9e60
Richard McIntyre <richard.mackstar@gmail.com>
2017-10-31 09:44:57 +0000

    Adding this a bid more streamlined.

--- src/answers/5_sum_numbers.js
+++ src/answers/5_sum_numbers.js
@@ -16,11 +16,9 @@ console.log(sum)
 //functional
 var sum = numbers.reduce(function(sum, value) {
     return sum + value
-})
+});
 
-console.log(sum)
+console.log(sum);
 
 //functional ES6
-var sum = numbers.reduce((sum, value) => {
-    return sum + value
-})
+var sum = numbers.reduce((sum, value) => (sum + value));
